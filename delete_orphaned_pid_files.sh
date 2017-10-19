56be0f1702e5c69b8219afe48500d6deac418148
Marin Nedea <marinnedea@users.noreply.github.com>
2017-10-19 09:50:36 +0300

    Update delete_orphaned_pid_files.sh
    Changed the "process_name" into a variable

--- delete_orphaned_pid_files.sh
+++ delete_orphaned_pid_files.sh
@@ -1,20 +1,23 @@
-// SCRIPT  : delete_orphaned_PID_files.sh
-// AUTHOR  : Marin Nedea										
-// WEBSITE : http://sysadmins.tech								
-// HOWTO   : Save the script to a location of your choice, chmod +x delete_orphaned_PID_files.sh,
-//		       then run it as root ( #./delete_orphaned_PID_files.sh) 	
+# SCRIPT  : delete_orphaned_PID_files.sh
+# AUTHOR  : Marin Nedea
+# WEBSITE : http://sysadmins.tech
+# HOWTO   : Save the script to a location of your choice, chmod +x delete_orphaned_PID_files.sh,
+#           and run it as root
+#           Make sure you changed example_process below with your actual process/service.
 
 #!/bin/bash
-processpid=$(ps axf | grep <process name here> | grep -v grep | awk '{print $1}')  #get the running <process name here> PID
-dir=/var/run/*<process name here>.pid                                     			   #get all <process name here>.pid files in /var/run/ directory
-for f in $dir                                                           			     #for each file in $dir
+
+process_name=example_process
+processpid=$(ps axf | grep $process_name | grep -v grep | awk '{print $1}') #get the running service/process name PID
+dir=/var/run/*$process_name.pid                                             #get all process .pid files in /var/run/ directory 
+for f in $dir                                                               #for each file in $dir
 do
-    pid=$(< "$f")                                                       			     #assign to var $pid the value in the file
+    pid=$(< "$f")                                                           #assign to var $pid the value in the file
       if [ "$processpid" -eq "$pid" ]
-            then 
-                  echo "<process name here> is running on PID $pid. Keeping $f intact."
-            else 
+            then
+                  echo "$process_name is running on PID $pid. Keeping $f intact."
+            else
                   echo "No process running on PID $pid. Deleting the $f file!"
-                  rm -f $f                                              			    #deleting the file corresponding to the value of $f
+                  rm -f $f                                                 #deleting the file corresponding to the $f
       fi
-done  
+done 
